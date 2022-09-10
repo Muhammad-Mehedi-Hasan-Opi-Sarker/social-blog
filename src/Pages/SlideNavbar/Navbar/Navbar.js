85de655e7e91d7ec0070162db3f2863da175a606
Mehedi Hasan Opi Sarker <opisarker99@gmail.com>
2022-09-10 16:28:26 +0600

    it's final navbar

--- src/Pages/SlideNavbar/Navbar/Navbar.js
+++ src/Pages/SlideNavbar/Navbar/Navbar.js
@@ -2,34 +2,45 @@ import React from 'react';
 import { HiOutlineHome, HiOutlinePencilAlt } from 'react-icons/hi';
 import { IoIosNotificationsOutline } from 'react-icons/io';
 import { BsBookmarks } from 'react-icons/bs';
-import { Link, Outlet } from "react-router-dom";
+import { Link } from "react-router-dom";
+import './Navbar.css';
 
 const Navbar = () => {
-    return (
-        <div>
-            <div className="navbar bg-base-100 lg:px-12 px-4">
-                <div className="flex-1">
-                    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-                </div>
+    const nav = <>
+        {/* home  */}
+        <li className='text-4xl lg:ml-3'><Link to='/home'><HiOutlineHome></HiOutlineHome></Link></li>
+        {/* notification  */}
+        <li className='text-5xl lg:ml-3'><a><IoIosNotificationsOutline></IoIosNotificationsOutline></a></li>
+        {/* bookmarks  */}
+        <li className='text-4xl lg:ml-3'><a><BsBookmarks></BsBookmarks></a></li>
+        {/* write post */}
+        <li className='text-4xl lg:ml-3'><Link to='/post'><HiOutlinePencilAlt></HiOutlinePencilAlt></Link></li>
 
-                <div className='flex-none navbar-center'>
-                <ul className="menu menu-horizontal p-0">
-                        
-                        {/* home  */}
-                        <li className='text-3xl lg:ml-5'><Link to='/home'><HiOutlineHome></HiOutlineHome></Link></li>
-                        {/* notification  */}
-                        <li className='text-4xl lg:ml-5'><a><IoIosNotificationsOutline></IoIosNotificationsOutline></a></li>
-                        {/* bookmarks  */}
-                        <li className='text-3xl lg:ml-5'><a><BsBookmarks></BsBookmarks></a></li>
-                        {/* write post */}
-                        <li className='text-3xl lg:ml-5'><Link to='/post'><HiOutlinePencilAlt></HiOutlinePencilAlt></Link></li>
-                        {/* porfile  */}
+    </>
+    return (
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        {nav}
                     </ul>
                 </div>
-                
-                <div className="flex-none gap-2 lg:ml-80">
+                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal p-0">
+                    {nav}
+                </ul>
+            </div>
+            <div className="navbar-end gap-2">
+
+                {/* profile pic and search  */}
+               
                     <div className="form-control">
-                        <input type="text" placeholder="Search" className="input input-bordered" />
+                        <input type="text" placeholder="Search" className="input input-bordered w-40" />
                     </div>
                     <div className="dropdown dropdown-end">
                         <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
@@ -48,7 +59,7 @@ const Navbar = () => {
                             <li><a>Logout</a></li>
                         </ul>
                     </div>
-                </div>
+                
             </div>
         </div>
     );
